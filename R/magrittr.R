daf2ba33c5a476e82d48f52f49ad90c79942d58c
JJ Allaire <jj@rstudio.org>
2016-06-22 16:09:25 -0400

    correct re-export for magrittr

--- R/magrittr.R
+++ R/magrittr.R
@@ -1,14 +1,5 @@
-#' magrittr forward-pipe operator
-#'
-#' Pipe an object forward into a function or call expression.
-#'
-#' @param lhs A value or the magrittr placeholder.
-#' @param rhs A function call using the magrittr semantics.
-#'
-#' @keywords internal
+
 #' @importFrom magrittr %>%
-#' @name %>%
-#' @rdname magrittr-forward-pipe
 #' @export
-NULL
+magrittr::`%>%`
 
