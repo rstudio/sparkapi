d56d1fb7ae151ec524f6393e3d0f143052730861
Javier Luraschi <javierluraschi@hotmail.com>
2016-07-08 17:23:12 -0700

    remove spark.session config parameter since session params use spark.sql

--- R/hive.R
+++ R/hive.R
@@ -29,13 +29,9 @@ create_hive_context_v2 <- function(sc) {
   # get config object
   conf <- invoke(session, "conf")
 
-  # apply spark.session. params
-  params <- connection_config(sc, "spark.session.")
-  apply_config(params, conf, "set")
-
   # apply spark.sql. params
   params <- connection_config(sc, "spark.sql.")
-  apply_config(params, conf, "set")
+  apply_config(params, conf, "set", "spark.sql.")
 
   # return session as hive context
   session
@@ -81,27 +77,26 @@ create_hive_context_v1 <- function(sc) {
 
   # apply configuration
   params <- connection_config(sc, "spark.sql.")
-  apply_config(params, hive_context, "setConf")
+  apply_config(params, hive_context, "setConf", "spark.sql")
 
   # return hive_context
   hive_context
 }
 
-apply_config <- function(params, object, method) {
-
+apply_config <- function(params, object, method, prefix) {
   lapply(names(params), function(paramName) {
     configValue <- params[[paramName]]
     if (is.logical(configValue)) {
       configValue <- if (configValue) "true" else "false"
     }
     else {
-      as.character(configValue)
+      configValue <- as.character(configValue)
     }
 
     invoke(
       object,
       method,
-      paramName,
+      paste0(prefix, paramName),
       configValue
     )
   })
