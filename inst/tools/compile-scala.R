9cfe62dd2c6f191952c7fb8487d70c0eb460d528
Javier Luraschi <javierluraschi@hotmail.com>
2016-07-22 22:05:33 -0700

    use versioned name for sparkapi jar

--- inst/tools/compile-scala.R
+++ inst/tools/compile-scala.R
@@ -1,6 +1,4 @@
 #!/usr/bin/env Rscript
 
 spark_home <- file.path(rappdirs::app_dir("spark", "rstudio")$cache(), "spark-1.6.1-bin-hadoop2.6")
-
-spark_compile("sparkapi", spark_home = spark_home)
 spark_compile("sparkapi", spark_home = spark_home)
