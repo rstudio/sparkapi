7227508fe81e6be3b83c32d874c4ec6980dccc24
Javier Luraschi <javierluraschi@hotmail.com>
2016-07-22 21:39:29 -0700

    use libs directory to auto detect spark versions by spark assembly

--- R/connection.R
+++ R/connection.R
@@ -107,13 +107,7 @@ spark_version <- function(sc) {
 
 spark_version_from_home_version <- function() {
   version <- Sys.getenv("SPARK_HOME_VERSION")
-  if (is.null(version)) {
-    stop(
-      "RELEASE file not found and the SPARK_HOME_VERSION ",
-      "environment variable is not set.")
-  }
-
-  numeric_version(version)
+  if (nchar(version) <= 0) NULL else version
 }
 
 #' Version of Spark for a SPARK_HOME directory
@@ -124,26 +118,58 @@ spark_version_from_home_version <- function() {
 #'
 #' @export
 spark_version_from_home <- function(spark_home) {
-  releaseFile <- file.path(spark_home, "RELEASE")
+  versionAttempts <- list(
+    useReleaseFile = function() {
+      versionedFile <- file.path(spark_home, "RELEASE")
+      if (file.exists(versionedFile)) {
+        releaseContents <- readLines(versionedFile)
 
-  if (!file.exists(releaseFile)) {
-    return(spark_version_from_home_version())
+        if (!is.null(releaseContents) && length(releaseContents) > 0) {
+          gsub("Spark | built.*", "", releaseContents[[1]])
+        }
+      }
+    },
+    useAssemblies = function() {
+      candidateVersions <- list(
+        list(path = "lib", pattern = "spark-assembly-([0-9\\.]*)-hadoop.[0-9\\.]*\\.jar"),
+        list(path = "yarn", pattern = "spark-([0-9\\.]*)-preview-yarn-shuffle\\.jar")
+      )
+
+      candidateFiles <- lapply(candidateVersions, function(e) {
+        c(e,
+          list(
+            files = list.files(
+              file.path(spark_home, e$path),
+              pattern = e$pattern
+            )
+          )
+        )
+      })
+
+      filteredCandidates <- Filter(function(f) length(f$files) > 0, candidateFiles)
+      if (length(filteredCandidates) > 0) {
+        valid <- filteredCandidates[[1]]
+        e <- regexec(valid$pattern, valid$files[[1]])
+        match <- regmatches(valid$files[[1]], e)
+        if (length(match) > 0 && length(match[[1]]) > 1) {
+          return(match[[1]][[2]])
+        }
+      }
+    },
+    useEnvironmentVariable = function() {
+      spark_version_from_home_version()
+    }
+  )
+
+  for (versionAttempt in versionAttempts) {
+    result <- versionAttempt()
+    if (length(result) > 0)
+      return(spark_version_clean(result))
   }
 
-  releaseContents <- readLines(releaseFile)
-
-  if (is.null(releaseContents) || length(releaseContents) == 0) {
-    spark_version_from_home_version()
-  }
-  else {
-    version <- gsub("Spark | built.*", "", releaseContents[[1]])
-
-    # Get rid of -preview and other suffix variations
-    version <- spark_version_clean(version)
-
-    # return numeric version
-    numeric_version(version)
-  }
+  stop(
+    "Failed to detect version from SPARK_HOME or SPARK_HOME_VERSION. ",
+    "Try passing the spark_version explicitly.")
 }
 
 #' Read configuration values for a connection
